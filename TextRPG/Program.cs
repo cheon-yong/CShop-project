d3cebd4f81fceeccc2736a846b374c8fbf56cbb1
yongjun <yes1312@naver.com>
2021-04-25 23:14:05 +0900

    TextRPG - 직업 고르기

--- TextRPG/Program.cs
+++ TextRPG/Program.cs
@@ -4,9 +4,51 @@ namespace TextRPG
 {
     class Program
     {
+        enum ClassType
+        {
+            None = 0,
+            Knight = 1,
+            Archer = 2,
+            Mage = 3
+        }
+
+       static ClassType ChooseClass()
+        {
+            Console.WriteLine("직업을 선택하세요!");
+            Console.WriteLine("[1] 기사");
+            Console.WriteLine("[2] 궁수");
+            Console.WriteLine("[3] 법사");
+
+            ClassType choice = ClassType.None;
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    choice = ClassType.Knight;
+                    break;
+                case "2":
+                    choice = ClassType.Archer;
+                    break;
+                case "3":
+                    choice = ClassType.Mage;
+                    break;
+            }
+
+            return choice;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ClassType choice = ClassType.None;
+
+            while (true)
+            {
+                choice = ChooseClass();
+                if (choice != ClassType.None)
+                    break;
+            }
+
         }
     }
 }
