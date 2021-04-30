ebb7578663fd2ac38d1de83cb4ca4c01ad6c69d8
yongjun <yes1312@naver.com>
2021-04-30 19:15:56 +0900

    TextRPG2 - 마무리

--- TextRPG2/Game.cs
+++ TextRPG2/Game.cs
@@ -15,6 +15,8 @@ namespace TextRPG2
     {
         private GameMode mode = GameMode.Lobby;
         private Player player = null;
+        private Monster monster = null;
+        private Random rand = new Random();
         public void Process()
         {
             switch (mode)
@@ -26,6 +28,7 @@ namespace TextRPG2
                     ProcessTown();
                     break;
                 case GameMode.Field:
+                    ProcessField();
                     break;
             }
         }
@@ -69,6 +72,83 @@ namespace TextRPG2
                     break;
                 case "2":
                     mode = GameMode.Lobby;
+                    break;
+            }
+        }
+
+        public void ProcessField()
+        {
+            Console.WriteLine("필드에 입장했습니다 !");
+            Console.WriteLine("[1] 싸우기");
+            Console.WriteLine("[2] 일정 확률로 마을 돌아가기");
+
+            CreateRandomMonster();
+
+            string input = Console.ReadLine();
+            switch(input)
+            {
+                case "1":
+                    ProcessFight();
+                    break;
+                case "2":
+                    TryEscape();
+                    break;
+            }
+        }
+
+        private void TryEscape()
+        {
+            int randValue = rand.Next(0, 101);
+            if (randValue < 33)
+            {
+                mode = GameMode.Town;
+            }
+            else
+            {
+                ProcessFight();
+            }
+        }
+
+        private void ProcessFight()
+        {
+            while (true)
+            {
+                int damage = player.GetAttack();
+                monster.OnDamaged(damage);
+                if (monster.IsDead())
+                {
+                    Console.WriteLine("승리했습니다");
+                    Console.WriteLine($"남은 체력 {player.GetHp()}");
+                    break;
+                }
+
+                damage = monster.GetAttack();
+                player.OnDamaged(damage);
+                if (player.IsDead())
+                {
+                    Console.WriteLine("패배했습니다");
+                    mode = GameMode.Lobby;
+                    break;
+                }
+            }
+        }
+
+        private void CreateRandomMonster()
+        {
+            int randValue = rand.Next(0, 3);
+            switch (randValue)
+            {
+                case 0:
+                    monster = new Slime();
+                    Console.WriteLine("슬라임이 생성되었습니다");
+                    break;
+                case 1:
+                    monster = new Orc();
+                    Console.WriteLine("오크가 생성되었습니다");
+                    break;
+                case 2:
+                    monster = new Skeleton();
+                    Console.WriteLine("스켈레톤이 생성되었습니다");
                     break;
             }
         }
